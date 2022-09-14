fa9c8bd83051a7517bdeab737904a346639b714d
1056481167@qq.com <1056481167@qq.com>
2022-09-14 15:12:11 +0800

    nacos commit

--- cloudalibaba-config-nacos-client3377/src/main/java/org/xwb/springcloud/controller/ConfigClientController.java
+++ cloudalibaba-config-nacos-client3377/src/main/java/org/xwb/springcloud/controller/ConfigClientController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -26,5 +27,10 @@ public class ConfigClientController {
     @GetMapping("configInfo1")
     public String getConfigInfo1() {
         return "serverPort1: " + serverPort + "\t\n\n configInfo1: " + configInfo;
+    }
+
+    @PostMapping("configInfoPost")
+    public String configInfoPost() {
+        return "serverPort1: " + serverPort + "\t\n\n configInfoPost: " + configInfo;
     }
 }
--- cloudalibaba-consumer-nacos-order83/src/main/java/org/xwb/springcloud/controller/OrderNacosController.java
+++ cloudalibaba-consumer-nacos-order83/src/main/java/org/xwb/springcloud/controller/OrderNacosController.java
@@ -2,9 +2,7 @@ package org.xwb.springcloud.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
@@ -20,5 +18,15 @@ public class OrderNacosController {
     @GetMapping("/consumer/payment/nacos/{id}")
     public String paymentInfo(@PathVariable("id") Long id) {
         return restTemplate.getForObject(nacosUserService + "/payment/nacos/" + id, String.class);
+    }
+
+    @PostMapping("/consumer/{id}")
+    public String paymentInfoPost(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(nacosUserService + "/paymentInfoPost/" + id, String.class);
+    }
+
+    @RequestMapping("/consumer/payment/{id}")
+    public String paymentInfoRequest(@PathVariable("id") Long id) {
+        return restTemplate.getForObject(nacosUserService + "/payment/" + id, String.class);
     }
 }
